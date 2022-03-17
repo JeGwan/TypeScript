d68ae24270a2198d213c2e3e65e84fd497e574f7
Sheetal Nandi <shkamat@microsoft.com>
2022-03-17 14:24:55 -0700

    No need to check for file existence before reading the d.ts file

--- tests/baselines/reference/tsbuild/sample1/initial-build/sample.js
+++ tests/baselines/reference/tsbuild/sample1/initial-build/sample.js
@@ -124,26 +124,24 @@ readFiles:: {
  "/src/core/anotherModule.ts": 1,
  "/src/core/index.ts": 1,
  "/src/core/some_decl.d.ts": 1,
+ "/src/core/anotherModule.d.ts": 2,
  "/src/logic/tsconfig.tsbuildinfo": 1,
  "/src/logic/index.ts": 1,
  "/src/core/index.d.ts": 1,
- "/src/core/anotherModule.d.ts": 1,
+ "/src/logic/index.d.ts": 2,
  "/src/tests/tsconfig.tsbuildinfo": 1,
  "/src/tests/index.ts": 1,
- "/src/logic/index.d.ts": 1
+ "/src/tests/index.d.ts": 1
 } 
 getModifiedTime:: {
  "/src/core/anotherModule.ts": 1,
  "/src/core/index.ts": 1,
  "/src/core/some_decl.d.ts": 1,
  "/src/core/anotherModule.js": 1,
- "/src/core/anotherModule.d.ts": 1,
  "/src/logic/index.ts": 1,
  "/src/logic/index.js": 1,
- "/src/logic/index.d.ts": 1,
  "/src/tests/index.ts": 1,
- "/src/tests/index.js": 1,
- "/src/tests/index.d.ts": 1
+ "/src/tests/index.js": 1
 }
 
 setModifiedTime:: {}
@@ -772,7 +770,6 @@ getModifiedTime:: {
  "/src/core/index.ts": 1,
  "/src/core/some_decl.d.ts": 1,
  "/src/core/anotherModule.js": 1,
- "/src/core/index.d.ts": 1,
  "/src/core/anotherModule.d.ts": 1,
  "/src/logic/index.ts": 1,
  "/src/logic/index.js": 1,
@@ -1400,9 +1397,9 @@ readFiles:: {
  "/src/logic/index.ts": 1,
  "/src/core/index.d.ts": 1,
  "/src/core/anotherModule.d.ts": 1,
+ "/src/logic/decls/index.d.ts": 2,
  "/src/tests/tsconfig.tsbuildinfo": 1,
  "/src/tests/index.ts": 1,
- "/src/logic/decls/index.d.ts": 1,
  "/src/tests/index.d.ts": 1
 } 
 getModifiedTime:: {
@@ -1420,7 +1417,7 @@ getModifiedTime:: {
  "/src/logic/index.ts": 1,
  "/src/logic/index.js": 1,
  "/src/logic/index.js.map": 1,
- "/src/logic/decls/index.d.ts": 2,
+ "/src/logic/decls/index.d.ts": 1,
  "/src/tests/index.ts": 1,
  "/src/tests/index.js": 1,
  "/src/tests/index.d.ts": 2,
